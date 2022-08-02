7cfa971f225fa402d53feaff6851126c0ccef93d
lucasdvillegas <97612405+lucasdvillegas@users.noreply.github.com>
2022-08-01 23:42:33 -0300

    Add files via upload

--- src/main/java/com/portfolio/ldv/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/ldv/Security/Controller/AuthController.java
@@ -15,6 +15,7 @@ import java.util.Set;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
